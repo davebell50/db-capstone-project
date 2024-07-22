cea64b279ebc1172f19c8129860ab543f5911814
davebell50 <168473582+davebell50@users.noreply.github.com>
2024-07-22 09:00:55 -0600

    Add files via upload

---
+++ Bookings Insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO Bookings (`BookingID`, `BookingDate`, `TableNumber`, `CustomerID`) VALUES ('1', '2024-07-01', 3, 5);
+INSERT INTO Bookings (`BookingID`, `BookingDate`, `TableNumber`, `CustomerID`) VALUES ('2', '2024-07-05', 2, 1);
+INSERT INTO Bookings (`BookingID`, `BookingDate`, `TableNumber`, `CustomerID`) VALUES ('3', '2024-07-07', 1, 3);
+INSERT INTO Bookings (`BookingID`, `BookingDate`, `TableNumber`, `CustomerID`) VALUES ('4', '2024-07-07', 4, 2);
+INSERT INTO Bookings (`BookingID`, `BookingDate`, `TableNumber`, `CustomerID`) VALUES ('5', '2024-07-09', 5, 4);
+SELECT * FROM Bookings;
